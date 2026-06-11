07ffd365da389dfa16e6bd2c43a9d7279f20ff4d
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 16:00:58 +0530

    Fix Realty suffix and update correct address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Ars Infrat
 
 Firm facts:
 - Firm: Ars Infratech
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Phase 2, Sector 54 / Sector 55 Area, Sahibzada Ajit Singh Nagar, Mohali
 - Phone: +91 XXXXX XXXXX
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
